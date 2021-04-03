6fdc08cf7da4c177f5e0ce8b606c206f938c53db
Ryan <ryan.hodgson@enco.net.au>
2021-04-03 10:30:02 +0800

    (btns) integrated buttons

--- canal/canal.ino
+++ canal/canal.ino
@@ -23,19 +23,23 @@ void setup()
 {  
   Serial.begin(9600);
 
-//  // setup LCD screen
-//  delay(1000);
-//  lcd.begin(16, 2);                          // put your LCD parameters here
-//  lcd.print("hello");
-//  lcd.setCursor(0,1);
-//  lcd.print("it works");
+  // set analog pins to digital
+  pinMode(A0, INPUT); // A0
+  pinMode(A1, INPUT); // A1
+  pinMode(A2, INPUT); // A2
+
+  // setup LCD screen
+  lcd.begin(16, 2); 
+  lcd.setCursor(0,0);     
 
   // setup SD card
   if (!SD.begin(4)) {
-    Serial.println("SD Card initialization failed!");
+    lcd.print("SD Card failed!");
+    Serial.println("SD Card failed!");
     while (1);
   }
-  Serial.println("SD Card initialization done.");
+  lcd.print("SD Card done.");
+  Serial.println("SD Card done.");
   
   // setup GPS
   ss.begin(GPSBaud);
@@ -53,21 +57,47 @@ void setup()
 
 void loop()
 { 
-//  while (ss.available() > 0)
-//    if (gps.encode(ss.read()))
-//      getInfo();
-//
-//  if (millis() > 5000 && gps.charsProcessed() < 10)
-//    doNotAvailable();
+  lcd.setCursor(0, 0);
+  lcd.print("Select mode.");
+
+  
+  if(digitalRead(14) == HIGH) {
+    lcd.clear();
+    lcd.print("Selected record.");
+
+    while(true) {
+      while (ss.available() > 0)
+        if (gps.encode(ss.read()))
+          getInfo();
+    
+      if (millis() > 5000 && gps.charsProcessed() < 10)
+        doNotAvailable();
+    }
+  } else if(digitalRead(15) == HIGH) {
+    lcd.clear();
+    lcd.print("Selected reset.");
+    removeFile(fileName);
+    while(1);
+  } else if(digitalRead(16) == HIGH) {
+    lcd.clear();
+    lcd.print("Selected upload.");
+    readFromFile(fileName);
+    while(1);
+  }
 }
 
 void doNotAvailable() {
   Serial.println(F("No GPS detected: check wiring."));
+  lcd.print("No GPS detected.");
+  
   while(true);
 }
 
 void getInfo()
 {
+  lcd.setCursor(0,0);
+  lcd.print("Retrieving info.");
+  
   // tell MPU-6050 we want to read
   Wire.beginTransmission(MPU_ADDR);
   Wire.write(0x3B);
@@ -89,41 +119,52 @@ void getInfo()
     char lng[6]; dtostrf(gps.location.lng(), 4, 6, lng);
 
     char dataString[50]; sprintf(dataString, "%s,%s,%lu,%d,%d,%d", lat, lng, (unsigned long) now(), accelerometer_x, accelerometer_y, accelerometer_z);
+    Serial.println(dataString);
     writeToFile(dataString);
   }
-
+  
   delay(1000);
+  lcd.clear();
 }
 
 void writeToFile(String row) {
-  Serial.println("Writing to file.");
   if(file) {
     file.println(row);
-    Serial.println(row);
-    Serial.println("Finished writing to file.");
+    lcd.setCursor(0,1);
+    lcd.println("Written to file.");
   } else {
     Serial.println("Error opening file.");
+    lcd.setCursor(0,0);
+    lcd.print("Error opening file.");
   }
 }
 
-//void removeFile(String fileName) {
-//   // filename must include extension
-//   if(SD.exists(fileName)) {
-//    Serial.println("Removing file.");
-//    SD.remove(fileName);
-//    Serial.println("Successfully removed file.");
-//  }
-//}
-//
-//void readFromFile(String fileName) {
-//  File file = SD.open(fileName);
-//  if (file) {
-//    Serial.println("Reading from file.");
-//    while (file.available()) {
-//      Serial.write(file.read());
-//    }
-//    file.close();
-//  } else {
-//    Serial.println("Error opening file.");
-//  }
-//}
+void removeFile(String fileName) {
+   lcd.clear();
+   
+   // filename must include extension
+   if(SD.exists(fileName)) {
+    Serial.println("Removing file.");
+    SD.remove(fileName);
+    Serial.println("Successfully removed file.");
+  } else {
+    Serial.println("File doesn't exist");
+    lcd.print("File doesn't exist");
+  }
+}
+
+void readFromFile(String fileName) {
+  lcd.clear();
+  
+  File file = SD.open(fileName);
+  if (file) {
+    Serial.println("Reading from file.");
+    while (file.available()) {
+      Serial.write(file.read());
+    }
+    file.close();
+  } else {
+    Serial.println("Error opening file.");
+    lcd.print("File doesn't exist");
+  }
+}
